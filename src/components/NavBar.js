48dd914222a236fd787484b14a8be9e0022161fe
MartinPucan <pucan.m@seznam.cz>
2021-02-01 19:47:07 +0100

    create new project - react app

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <h3>Menu</h3>
+            <h3>Contacts</h3>
+        </nav>
+    );
+}
+
+export default NavBar;
