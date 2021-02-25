d590e30dfd9c7d6b7d003264615ba503fc35c275
MartinPucan <pucan.m@seznam.cz>
2021-02-25 23:59:16 +0100

    added container for footer

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer__container">
+            <section className="footer__wrapper">
+                <div className="col-4">
+                    <h3>Items 1</h3>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                </div>
+                <div className="col-4">
+                    <h3>Items 2</h3>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                </div>
+                <div className="col-4">
+                    <h3>Items 3</h3>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                    <div>Lorem</div>
+                </div>
+                <div className="col-4">
+                    <h3>Items 4</h3>
+                </div>
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
