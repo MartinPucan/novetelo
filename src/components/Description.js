29bf13254ca687d042e4e8b830d4f47a5b3010f5
MartinPucan <pucan.m@seznam.cz>
2021-05-21 16:44:49 +0200

    missing dot

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ const Description = () => {
     return (
         <section className="container-description">
             <div className="quote">
-                <q>Until you start believing in yourself you ain't gonna have a life</q>
+                <q>Until you start believing in yourself you ain't gonna have a life.</q>
             </div>
             <div className="main-image">
                 <img src={adamImage} alt="adam" loading="lazy" />
